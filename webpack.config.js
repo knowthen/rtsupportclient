438c1bbb72731ed528bbb946ebb36ba001da065e
James Moore <james@knowthen.com>
2016-05-10 16:22:49 -0400

    update webpack for babel 6

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ module.exports = {
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
       }
     ]
   }
